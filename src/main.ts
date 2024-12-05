e6a84f85bc54b346e533609768bd0ad1b2fc4fe8
Francisco Poblete <36689098+loppillo@users.noreply.github.com>
2024-12-05 08:34:30 -0300

    main

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('/cert/private-key.pem'),   // Ruta a tu clave privada
+    cert: fs.readFileSync('/cert/certificate.pem'),  // Ruta a tu certificado
+  };
+
   const port = process.env.PORT || 4000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    httpsOptions
+  });
+  
   app.setGlobalPrefix("api/v1");
   app.enableCors();
   app.useGlobalPipes(
@@ -20,5 +29,12 @@ async function bootstrap() {
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   await app.listen(port);
   
+  
+
+  
+
+  await app.listen(3000); 
+
+
 }
 bootstrap();
