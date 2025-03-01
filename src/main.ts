21ce47a1d658e07a209596990e4cec29f5bf5b3f
Francisco Poblete <36689098+loppillo@users.noreply.github.com>
2025-03-01 01:25:44 -0300

    reparacion

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,12 @@ const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
   
   app.setGlobalPrefix("api/v1");
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://fenfurnacional.cl','http://localhost:4000', 'https://fenfurnacional.com','https://www.fenfurnacional.cl'], // Dominios permitidos
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    allowedHeaders: 'Content-Type,Authorization', // Cabeceras permitidas
+    credentials: true, // Permite que se envíen cookies de sesión
+  });
   
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
